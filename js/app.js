c158dd5a9fc2227b55b83bf3d3721957df2e3739
ataurrahman1624-bit <ataurrahman1624@gmail.com>
2026-02-06 20:09:49 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -76,6 +76,24 @@ var swiper = new Swiper(".swiperReview", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
+  breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 20,
+        },
+      },
 
     });
    
@@ -95,3 +113,4 @@ var swiper = new Swiper(".swiperReview", {
     ));
   });
   // countdown jquery file ends here
+
